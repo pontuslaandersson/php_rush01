cc6984b80701ae5e2b1c4972f864fb1db25512d2
Rori Ivanovo <zraunio@student.hive.fi>
2020-11-22 17:38:23 +0200

    docs and index changes

--- login.php
+++ login.php
@@ -7,7 +7,7 @@ if ($_POST['login1'] && $_POST['login2'] && $_POST['login3'] && $_POST['login4']
 	$_SESSION["logged_in_user"]["key2"] = $_POST["login2"];
 	$_SESSION["logged_in_user"]["key3"] = $_POST["login3"];
 	$_SESSION["logged_in_user"]["key4"] = $_POST["login4"];
-	if ($_POST['submit'] === 'OK')
+	if ($_POST['submit'] === 'CONNECT')
 		header("Location: D08/play.html");
 		?>
 	<!DOCTYPE HTML>
@@ -18,14 +18,15 @@ if ($_POST['login1'] && $_POST['login2'] && $_POST['login3'] && $_POST['login4']
 <LINK href="index.css" rel="stylesheet">
 </HEAD>
 <BODY>
-	<p><a id ="name">Welcome <?php echo $_SESSION['logged_in_user']['key1']?><p</a>
-	<p><a id ="name">Welcome <?php echo $_SESSION['logged_in_user']['key2']?><p</a>
-	<p><a id ="name">Welcome <?php echo $_SESSION['logged_in_user']['key3']?><p</a>
-	<p><a id ="name">Welcome <?php echo $_SESSION['logged_in_user']['key4']?><p</a>
+	<p><a><H1>Welcome <?php echo $_SESSION['logged_in_user']['key1']?></H1><p</a>
+	<p><a><H1>Welcome <?php echo $_SESSION['logged_in_user']['key2']?></H1><p</a>
+	<p><a><H1>Welcome <?php echo $_SESSION['logged_in_user']['key3']?></H1><p</a>
+	<p><a><H1>Welcome <?php echo $_SESSION['logged_in_user']['key4']?></H1><p</a>
+	<br />
 	<H1>FIGHT!</H1>
-	<IMG id="picture" 
+	<DIV id="picture" ><IMG
 	SRC="https://cdn.dribbble.com/users/375867/screenshots/4053894/2.png"
-	ALT="SHIP!" TITLE="SHIP!">
+	ALT="SHIP!" TITLE="SHIP!"></DIV>
 </BODY>
 </HTML>
 	<?PHP
